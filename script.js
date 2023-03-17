0d0e17a8e5955611c430acb6aecbadd2b001a49f
MartinaTrrs <123593314+MartinaTrrs@users.noreply.github.com>
2023-03-17 20:58:07 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let botonnoseve = document.getElementById('botonnoseve');
-let noseve = document.getElementById('noseve');
-
-botonnoseve.addEventListener('click', toggletext);
-
-function toggletext() {
-    noseve.classList.toggle('show');
-}
